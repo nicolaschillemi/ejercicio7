03b53650bdca1bc8db660e93e35f187fe156f708
nicolaschillemi <nicolaschillemi@hotmail.com>
2019-05-03 20:18:42 -0300

    Multiplicacion de dos valores

---
+++ main.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+void main()
+
+{
+    int numero1;
+    int numero2;
+    int resultado;
+
+    printf("Ingresar primer nuemero\n\n");
+    scanf("%i",&numero1);
+
+    printf("Ingresar el segundo numero\n\n");
+    scanf("%i",&numero2);
+
+    resultado = numero1 * numero2;
+
+    printf("El resultado es %i",resultado);
+
+
+
+}
